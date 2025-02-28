b345eb223dc9a834d76be722e08a47930f860667
Ginaronch <alexandr.yarosh1@gmail.com>
2025-02-28 15:52:37 +0100

    version 1.0

--- 6.3.py
+++ 6.3.py
@@ -1,8 +1,8 @@
-user_input = input()
+user_input = input("Введите число: ")
 answer = int(user_input)
 while answer > 9:
     answer = 1
     for number in user_input:
         answer *= int(number)
         user_input = str(answer)
-print(answer)
+print("Ответ: ",answer)
